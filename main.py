e4c5529a143ecc45b5be422870a19f2316b76d30
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-02-24 17:53:38 +0800

    feature add

--- main.py
+++ main.py
@@ -20,6 +20,19 @@ def token():
         log.error("No token.txt!")
         return ""
 
+def starttxt():
+    try:
+        with open("start.txt","r") as f:
+            return f.read().rstrip('\n')
+    except FileNotFoundError:
+        log.warning("No start.txt, using the example.")
+        try:
+            with open("start.txt.example","r") as f:
+                return f.read().rstrip('\n')
+        except FileNotFoundError:
+            log.warning("No start.txt.example!")
+            return ""
+
 def logc():
     try:
         with open("logc.txt","r") as f:
@@ -35,7 +48,7 @@ def GetCMDCallBack(cname,rcont,loc,bot):
         bot.send_message(loc,disable_notification=True, text="Type: Command Log\nUser ID: {}\nFirst name: {}\nUsed Command: /{}".format(update.message.from_user.id,update.message.from_user.first_name,cname))
     return CMDCB
 
-helptxt = "Source code: https://github.com/Emojigit/tg_cmdreply\nLicense: GPLv3"
+helptxt = "Source code: https://github\.com/Emojigit/tg\_cmdreply\nLicense: GPLv3"
 
 def main():
     tok = token()
@@ -58,15 +71,20 @@ def main():
     for key,txt in jsondata().items():
         log.info("Registering {} command with '{}'".format(key,txt))
         try:
-            if key == "help":
-                raise ValueError("The `help` command is for special use, please do not register it.")
+            if key == "help" or key == "ping" or key == "start":
+                raise ValueError("The `{}` command is for special use, please do not register it.".format(key))
             dp.add_handler(CommandHandler(key, GetCMDCallBack(key,txt,loc,bot)))
         except ValueError as error:
             log.warning("Not register {} because error".format(key), exc_info=True)
             #log.exception(error)
             continue
         FCL.insert(-1,key)
-    dp.add_handler(CommandHandler("help", GetCMDCallBack("help","I have these commands: {}\n{}".format("/" + " /".join(map(str, FCL))),loc,bot,helptxt)))
+    dp.add_handler(CommandHandler("ping", GetCMDCallBack("ping","pong",loc,bot)))
+    dp.add_handler(CommandHandler("start", GetCMDCallBack("start",starttxt(),loc,bot)))
+    FCL.insert(-1,"help")
+    FCL.insert(-1,"ping")
+    FCL.insert(-1,"start")
+    dp.add_handler(CommandHandler("help", GetCMDCallBack("help","I have these commands: {}\n{}".format("/" + " /".join(map(str, FCL)),helptxt),loc,bot)))
     log.info("Finally i registered these commands: {}".format(str(FCL)))
     updater.start_polling()
     log.info("Started the bot! Use Ctrl-C to stop it.")
